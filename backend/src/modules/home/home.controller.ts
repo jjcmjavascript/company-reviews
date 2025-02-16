e2ae1d0933248a7a01830346e4552dbc0da33d5e
jjcmjavascript <jjcmjavascript@gmail.com>
2025-02-16 14:25:59 -0300

    refactor: eliminar decorador HasRoles del controlador HomeController

--- backend/src/modules/home/home.controller.ts
+++ backend/src/modules/home/home.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { HomeIndexResponse } from './home.interfaces';
 import { HomeIndexService } from './service/home-index.service';
-import { HasRoles } from '@shared/decorators/user-roles.decorator';
-import { Roles } from '@shared/services/permission/types/roles.enum';
 
 @Controller('/')
 export class HomeController {
   constructor(private readonly homeRepository: HomeIndexService) {}
 
   @Get()
-  @HasRoles(Roles.Admin)
   async index(): Promise<HomeIndexResponse[]> {
     const result = await this.homeRepository.execute();
 
